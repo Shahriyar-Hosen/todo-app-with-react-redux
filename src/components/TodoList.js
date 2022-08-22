168439c8768ff552aa45dfdda04879c94da320d5
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-22 08:54:12 +0600

    todo setup done

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 const TodoList = () => {
+  const todos = useSelector((store) => store.todos);
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      <Todo />
+      {todos.map((todo) => (
+        <Todo todo={todo} key={todo.id} />
+      ))}
     </div>
   );
 };
