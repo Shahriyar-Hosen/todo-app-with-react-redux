cc506062f8a83c8ce16c9dc5606ba1e448bf280f
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-22 13:36:22 +0600

    filter By Status Change & filter By Colors Change

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,39 @@ import { useSelector } from "react-redux";
 import Todo from "./Todo";
 
 const TodoList = () => {
-  const todos = useSelector((store) => store.todos);
+  const { todos, filters } = useSelector((store) => store);
+
+  const { status, colors } = filters;
+
+  // filter By Status Change
+  const filterByStatusChange = (todo) => {
+    switch (status) {
+      case "Complete":
+        return todo.completed;
+
+      case "Incomplete":
+        return !todo.completed;
+      default:
+        return true;
+    }
+  };
+
+  // filter By Colors Change
+  const filterByColorsChange = (todo) => {
+    if (colors.length > 0) {
+      return colors.includes(todo?.color);
+    }
+    return true;
+  };
 
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {todos.map((todo) => (
-        <Todo todo={todo} key={todo.id} />
-      ))}
+      {todos
+        .filter(filterByStatusChange)
+        .filter(filterByColorsChange)
+        .map((todo) => (
+          <Todo todo={todo} key={todo.id} />
+        ))}
     </div>
   );
 };
