aa91449c5204fc0abb00ea71de7e3d19dae2e012
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-22 12:35:35 +0600

    handle Status Change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { statusChanged } from "../redux/filters/action";
 
 const numberOfTask = (no_of_task) => {
   switch (no_of_task) {
@@ -15,18 +16,42 @@ const numberOfTask = (no_of_task) => {
 
 const Footer = () => {
   const todos = useSelector((state) => state.todos);
+  const filters = useSelector((state) => state.filters);
+
+  const dispatch = useDispatch();
 
   const todoRemaining = todos.filter((todo) => !todo.completed).length;
+
+  const { status, color } = filters;
+
+  const handleStatusChange = (status) => {
+    dispatch(statusChanged(status));
+  };
 
   return (
     <div className="mt-4 flex justify-between text-xs text-gray-500">
       <p>{numberOfTask(todoRemaining)} left</p>
       <ul className="flex space-x-1 items-center text-xs">
-        <li className="cursor-pointer font-bold">All</li>
+        <li
+          className={`cursor-pointer ${status === "All" && "font-bold"}`}
+          onClick={() => handleStatusChange("All")}
+        >
+          All
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Incomplete</li>
+        <li
+          className={`cursor-pointer ${status === "Incomplete" && "font-bold"}`}
+          onClick={() => handleStatusChange("Incomplete")}
+        >
+          Incomplete
+        </li>
         <li>|</li>
-        <li className="cursor-pointer">Complete</li>
+        <li
+          className={`cursor-pointer ${status === "Complete" && "font-bold"}`}
+          onClick={() => handleStatusChange("Complete")}
+        >
+          Complete
+        </li>
         <li></li>
         <li></li>
         <li className="h-3 w-3 border-2 border-green-500 md:hover:bg-green-500 rounded-full cursor-pointer bg-green-500"></li>
