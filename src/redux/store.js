fe577e7294a894ebad9027b6d40c00ab8baa0e48
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-25 23:37:55 +0600

    data loaded in middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-
-const store = createStore(rootReducer, composeWithDevTools() );
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
