c3a6689ac292e355f826bb0376f01d9baa80d3b9
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-25 23:57:08 +0600

    addTodo thunk

---
+++ src/redux/todos/thunk/addTodu.js
@@ -0,0 +1,22 @@
+import { added } from "../action";
+
+const addTodo = (todoText) => {
+  return async (dispatch) => {
+    const res = await fetch("http://localhost:9000/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        text: todoText,
+        completed: false,
+      }),
+      headers: {
+        "component-type": "application/json; charset=UTF-8",
+      },
+    });
+    const todo = await res.json();
+
+    //   dispatch
+    dispatch(added(todo.text));
+  };
+};
+
+export default addTodo;
--- src/redux/todos/thunk/fetchTodos.js
+++ src/redux/todos/thunk/fetchTodos.js
@@ -1,13 +1,11 @@
 import { loaded } from "../action";
 
-const fetchTodos = async (dispatch, getState) => {
+const fetchTodos = async (dispatch) => {
   const res = await fetch("http://localhost:9000/todos");
   const todos = await res.json();
 
   //   dispatch
   dispatch(loaded(todos));
 };
-
-fetchTodos();
 
 export default fetchTodos;
